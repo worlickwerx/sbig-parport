d9a684a6590c1d3893c6225dce9c184296a9a85f
Jim Garlick <garlick.jim@gmail.com>
2020-06-19 15:03:01 -0700

    driver: make other files GPL-2.0-only.
    Problem: the code I added to this project was designated
    GPL-2.0-or-later but DL released their code under GPL-2.0-only.
    
    Update license on my code to GPL-2.0-only to avoid any confusion.

--- driver/module.c
+++ driver/module.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0-only
 
 /* SBIG astronomy camera parallel port driver main
  *
--- driver/sbiglpt_module.h
+++ driver/sbiglpt_module.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0-only */
 
 #ifndef _SBIGLPT_MODULE_H
 #define _SBIGLPT_MODULE_H
